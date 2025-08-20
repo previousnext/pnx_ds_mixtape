5aeeaf2f64d066c0e7b11f3a0d68646ff5628369
dpi <pro@danielph.in>
2025-08-20 15:56:32 +0800

    ✨ Local Image Generator (#69)
    Add a local image generator for dumper and Drupal so we don't need to rely on flakey/rate-limited Picsum

--- src/PnxDsMixtapeCompilerPass.php
+++ src/PnxDsMixtapeCompilerPass.php
@@ -4,19 +4,21 @@ declare(strict_types=1);
 
 namespace Drupal\pnx_ds_mixtape;
 
+use Drupal\pnx_ds_mixtape\EventListener\PrimeImageGenerator;
 use PreviousNext\Ds\Mixtape\List\MixtapeLists;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-/**
- * Service provider.
- */
 final class PnxDsMixtapeCompilerPass implements CompilerPassInterface {
 
   public function process(ContainerBuilder $container): void {
     $pintoLists = $container->getParameter('pinto.lists');
     \array_push($pintoLists, ...MixtapeLists::Lists);
     $container->setParameter('pinto.lists', $pintoLists);
+    $container
+      ->register(PrimeImageGenerator::class, PrimeImageGenerator::class)
+      ->addTag('event_subscriber')
+      ->setPublic(TRUE);
   }
 
 }
